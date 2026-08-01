d68da84d334e171ec3ca299166f2bf872f996c30
jasitharan <49607856+jasitharan@users.noreply.github.com>
2026-08-01 21:43:56 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,38 @@
+class MinStack {
+
+    Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        this.stack.push(val);
+    }
+    
+    public void pop() {
+        this.stack.pop();
+    }
+    
+    public int top() {
+        return this.stack.peek();
+    }
+    
+    public int getMin() {
+        Stack<Integer> tmp = new Stack();
+
+         int mini = stack.peek();
+
+        while (!stack.isEmpty()) {
+            mini = Math.min(mini, stack.peek());
+            tmp.push(stack.pop());
+        }
+                   
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
+        }
+
+        return mini;
+
+    }
+}
